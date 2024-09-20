d725d23a1f3c0fcb0127fb86b49aeaf7fdc4e49c
RoeserHe825 <RoeserHe825@northwested.org>
2024-09-20 13:43:28 -0400

    Update concat_mc.py

---
+++ concat_mc.py
@@ -0,0 +1,28 @@
+# Henry Roeser
+# Sep 20 2024
+# Concatenation Check
+
+
+# Task 1
+
+first_name = "Henry"
+fav_food = "sushi"
+
+print("Hello my name is" + " " + first_name + " " + "and my favorite food is" + " " + fav_food + ".")
+
+
+# Task 2
+
+print("I have learned when doing concatenation to put quotation marks around strings and between concatenation operators for a space.")
+
+print("Another thing i've learned about concatenation is to not put any brackets around variables.")
+
+print("My final thing i've learned about concatenation is to put concatenation operators between strings and variables.")
+
+
+# Task 3
+
+fries_price = 2.99
+menu_item = "chicken fries"
+
+print("At Burger King the" + " " + menu_item + " " + "cost $" + str(fries_price))
